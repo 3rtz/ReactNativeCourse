7c6e805f3d2a065149ff47e91f369951ff327715
sicks <robert.sicks@itwm.fraunhofer.de>
2020-04-19 17:32:03 +0200

    finished first tast

--- project0/script.js
+++ project0/script.js
@@ -10,5 +10,27 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  var node = document.createElement("LI")
+  node.setAttribute('class','todo-container')
+  var textnode = document.createElement("INPUT")
+  textnode.setAttribute('class', 'todo-text')
+  textnode.setAttribute('type', 'text')
+  textnode.setAttribute('value', 'new Todo')
+  var checkboxnode = document.createElement('INPUT')
+  checkboxnode.setAttribute('type','checkbox')
+  checkboxnode.setAttribute('class','todo-checkbox')
+  checkboxnode.addEventListener("click", function(){onClick(this)})
+  node.appendChild(textnode)
+  node.appendChild(checkboxnode)
+  list.appendChild(node)
+
+  itemCountSpan.textContent = parseInt(itemCountSpan.textContent) +1
+}
+
+function onClick(id){
+  if (id.checked) {
+    uncheckedCountSpan.innerHTML = parseInt(uncheckedCountSpan.innerHTML) +1
+  } else {
+    uncheckedCountSpan.innerHTML = parseInt(uncheckedCountSpan.innerHTML) -1
+  }
 }
