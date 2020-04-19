5dac7f35d697898b1dc4b289447cca691464ecd7
sicks <robert.sicks@itwm.fraunhofer.de>
2020-04-19 17:51:36 +0200

    done with the task

--- project0/script.js
+++ project0/script.js
@@ -10,18 +10,24 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  var node = document.createElement("LI")
+  const node = document.createElement("LI")
   node.setAttribute('class','todo-container')
-  var textnode = document.createElement("INPUT")
+  const textnode = document.createElement("INPUT")
   textnode.setAttribute('class', 'todo-text')
   textnode.setAttribute('type', 'text')
   textnode.setAttribute('value', 'new Todo')
-  var checkboxnode = document.createElement('INPUT')
+  const checkboxnode = document.createElement('INPUT')
   checkboxnode.setAttribute('type','checkbox')
   checkboxnode.setAttribute('class','todo-checkbox')
   checkboxnode.addEventListener("click", function(){onClick(this)})
+  const delete_btn = document.createElement('BUTTON')
+  delete_btn.setAttribute('class', 'todo-delete')
+  delete_btn.innerHTML = 'del'
+  delete_btn.addEventListener("click", function(){delNode(this)})
+
   node.appendChild(textnode)
   node.appendChild(checkboxnode)
+  node.appendChild(delete_btn)
   list.appendChild(node)
 
   itemCountSpan.textContent = parseInt(itemCountSpan.textContent) +1
@@ -33,4 +39,7 @@ function onClick(id){
   } else {
     uncheckedCountSpan.innerHTML = parseInt(uncheckedCountSpan.innerHTML) -1
   }
+}
+function delNode(id){
+  list.removeChild(id.parentNode)
 }
